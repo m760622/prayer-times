7d7d99fdacc2bbd222acf889ca1bb6be3bf41209
hammam <hammamo2010@gmail.com>
2018-05-02 13:02:00 +0300

    change the tableview to stack of view

--- prayer times/Controller/ViewController.swift
+++ prayer times/Controller/ViewController.swift
@@ -8,19 +8,8 @@
 
 import UIKit
 import ChameleonFramework
-class ViewController: UIViewController, UITableViewDelegate , UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = prayersTableView.dequeueReusableCell(withIdentifier: "prayer", for: indexPath)
-      
-        cell.backgroundColor = UIColor(white: 1, alpha: 0)
-        
-      //  cell.backgroundColor = UIColor(gradientStyle: .leftToRight , withFrame: .init(x: 0, y: 0, width: cell.frame.width, height: cell.frame.height), andColors: [UIColor(hexString: "9FDEE6"),UIColor(hexString: "539AA7")])
-        return cell
-    }
+class ViewController: UIViewController{
+ 
     
     @IBOutlet weak var prayersTableView: UITableView!
     
@@ -28,10 +17,7 @@ class ViewController: UIViewController, UITableViewDelegate , UITableViewDataSou
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        prayersTableView.delegate = self
-        prayersTableView.dataSource = self
-        prayersTableView.rowHeight = 98
-        prayersTableView.backgroundColor = UIColor(white: 1, alpha: 0)
+    
         
         view.backgroundColor = UIColor(gradientStyle: .topToBottom, withFrame: .init(x: 0, y: 0, width: view.frame.width, height: view.frame.height), andColors: [UIColor(hexString: "9FDEE6"),UIColor(hexString: "539AA7")])
     }
