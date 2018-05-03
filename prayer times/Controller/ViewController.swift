82f3519a4fbfe6cb475e2ccb3abfc0cab34958ce
hammam <hammamo2010@gmail.com>
2018-05-03 12:29:33 +0300

    design home page

--- prayer times/Controller/ViewController.swift
+++ prayer times/Controller/ViewController.swift
@@ -10,8 +10,35 @@ import UIKit
 import ChameleonFramework
 class ViewController: UIViewController{
  
+    @IBOutlet weak var fajerPrayer: UILabel!
+
     
     @IBOutlet weak var prayersTableView: UITableView!
+    
+    @IBOutlet weak var fajerPrayerTime: UILabel!
+    
+    
+    @IBOutlet weak var dohorPrayerTime: UILabel!
+    
+    @IBOutlet weak var aserPrayer: UILabel!
+    
+    @IBOutlet weak var dohorPrayer: UILabel!
+    
+    @IBOutlet weak var maghrebPrayer: UILabel!
+    
+    @IBOutlet weak var aserPrayerTime: UILabel!
+    
+    @IBOutlet weak var nextPrayer: UILabel!
+    
+    @IBOutlet weak var maghrebPrayerTime: UILabel!
+    
+    @IBOutlet weak var nextPrayerTime: UILabel!
+    
+    @IBOutlet weak var ishaPrayer: UILabel!
+    
+    @IBOutlet weak var ishaPrayerTime: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +47,13 @@ class ViewController: UIViewController{
     
         
         view.backgroundColor = UIColor(gradientStyle: .topToBottom, withFrame: .init(x: 0, y: 0, width: view.frame.width, height: view.frame.height), andColors: [UIColor(hexString: "9FDEE6"),UIColor(hexString: "539AA7")])
+    //    prayersLabel.backgroundColor = UIColor(hexString: "061F2A").withAlphaComponent(0.2)
+        
+    //    dohorLabel.backgroundColor = UIColor(hexString: "061F2A").withAlphaComponent(0.2)
+        nextPrayer.backgroundColor = UIColor(hexString: "061F2A").withAlphaComponent(0.2)
+        
+        nextPrayerTime.backgroundColor = UIColor(hexString: "061F2A").withAlphaComponent(0.2)
+        
     }
 
     
