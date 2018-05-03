2f8f15a69941b928b6a4d331acf6a121044a4bfc
باسم امام <basim@iMac-alkhas-b-Mahmoud.local>
2018-05-03 12:36:08 +0300

    api

--- prayer times/Controller/ViewController.swift
+++ prayer times/Controller/ViewController.swift
@@ -7,7 +7,10 @@
 //
 
 import UIKit
+
 import ChameleonFramework
+import Alamofire
+import SwiftyJSON
 class ViewController: UIViewController{
  
     
@@ -16,13 +19,35 @@ class ViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        
+        
+       API()
     
         
         view.backgroundColor = UIColor(gradientStyle: .topToBottom, withFrame: .init(x: 0, y: 0, width: view.frame.width, height: view.frame.height), andColors: [UIColor(hexString: "9FDEE6"),UIColor(hexString: "539AA7")])
     }
 
-    
+    func API (){
+        
+        let urls = "http://api.islamhouse.com/v1/XXXXXXXXX/services/praytime/get-times/2014/08/13/Makkah/30.0599153/31.2620199/+3/json"
+        Alamofire.request(urls, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+                    
+                   
+                    let cat : JSON = JSON(response.result.value!)
+                   
+                    
+                    print(cat)
+                    
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    
+                }
+        }
+        
+        
+    }
 
 
 }
