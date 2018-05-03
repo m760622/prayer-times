2fdfa3f6a1f05fa5a9fc6b6ce4439ce8a0063674
hammam <hammamo2010@gmail.com>
2018-05-03 15:43:43 +0300

    desighn the second segue

--- prayer times/Controller/ViewController.swift
+++ prayer times/Controller/ViewController.swift
@@ -10,49 +10,34 @@ import UIKit
 import ChameleonFramework
 class ViewController: UIViewController{
  
-    @IBOutlet weak var fajerPrayer: UILabel!
-
-    
-    @IBOutlet weak var prayersTableView: UITableView!
-    
-    @IBOutlet weak var fajerPrayerTime: UILabel!
-    
-    
-    @IBOutlet weak var dohorPrayerTime: UILabel!
-    
-    @IBOutlet weak var aserPrayer: UILabel!
-    
-    @IBOutlet weak var dohorPrayer: UILabel!
-    
-    @IBOutlet weak var maghrebPrayer: UILabel!
-    
-    @IBOutlet weak var aserPrayerTime: UILabel!
-    
+    //MARK: Labels
+    //MARK: Prayers
     @IBOutlet weak var nextPrayer: UILabel!
-    
-    @IBOutlet weak var maghrebPrayerTime: UILabel!
-    
-    @IBOutlet weak var nextPrayerTime: UILabel!
-    
+    @IBOutlet weak var fajerPrayer: UILabel!
+    @IBOutlet weak var dohorPrayer: UILabel!
+    @IBOutlet weak var aserPrayer: UILabel!
+    @IBOutlet weak var maghrebPrayer: UILabel!
     @IBOutlet weak var ishaPrayer: UILabel!
-    
+    //MARK: Times of prayers
+    @IBOutlet weak var nextPrayerTime: UILabel!
+    @IBOutlet weak var fajerPrayerTime: UILabel!
+    @IBOutlet weak var dohorPrayerTime: UILabel!
+    @IBOutlet weak var aserPrayerTime: UILabel!
+    @IBOutlet weak var maghrebPrayerTime: UILabel!
     @IBOutlet weak var ishaPrayerTime: UILabel!
     
     
-    
+    //MARK:- viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
-    
         
+        //gradient color of the background
         view.backgroundColor = UIColor(gradientStyle: .topToBottom, withFrame: .init(x: 0, y: 0, width: view.frame.width, height: view.frame.height), andColors: [UIColor(hexString: "539AA7"),UIColor(hexString: "9FDEE6")])
-    //    prayersLabel.backgroundColor = UIColor(hexString: "061F2A").withAlphaComponent(0.2)
+   
         
-    //    dohorLabel.backgroundColor = UIColor(hexString: "061F2A").withAlphaComponent(0.2)
-        nextPrayer.backgroundColor = UIColor(hexString: "061F2A").withAlphaComponent(0.2)
-        
-        nextPrayerTime.backgroundColor = UIColor(hexString: "061F2A").withAlphaComponent(0.2)
+        //make the labels black to let the user destinguesh between prayers
+        dohorPrayer.backgroundColor = UIColor(hexString: "061F2A").withAlphaComponent(0.2)
+        dohorPrayerTime.backgroundColor = UIColor(hexString: "061F2A").withAlphaComponent(0.2)
         
     }
 
